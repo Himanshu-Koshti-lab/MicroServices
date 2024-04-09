9ab1536a5513e2fad0057009273e1ce5eb0a3786
Himanshu Koshti <Himfeedback74@gmail.com>
2024-04-09 22:49:34 +0530

    Enable eureka server and register all three application.

--- product-composite-service/src/main/java/com/microservice/product/composite/service/ProductCompositeServiceApplication.java
+++ product-composite-service/src/main/java/com/microservice/product/composite/service/ProductCompositeServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservice.product.composite.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ProductCompositeServiceApplication {
 
 	public static void main(String[] args) {
