f80824f67e3ade36749b30ad183f8e00da7a312a
Himanshu Koshti <Himfeedback74@gmail.com>
2024-04-09 12:05:33 +0530

    Adding common resource

---
+++ microservice-core/src/test/java/com/core/microservicecore/MicroserviceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.microservicecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
