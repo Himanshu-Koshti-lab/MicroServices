2a01b16ecd7a661898134e7b42745a1411c45de5
Himanshu Koshti <Himfeedback74@gmail.com>
2024-04-09 11:57:25 +0530

    Initial Setup

---
+++ recommedation-service/src/main/java/com/microservice/recommedation/RecommedationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.recommedation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommedationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommedationServiceApplication.class, args);
+	}
+
+}
---
+++ recommedation-service/src/test/java/com/microservice/recommedation/RecommedationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.recommedation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommedationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
