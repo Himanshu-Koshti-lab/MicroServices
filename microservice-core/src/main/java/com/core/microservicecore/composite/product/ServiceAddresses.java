a16781f1b2768a2987572aeebe58a98373698531
Himanshu Koshti <Himfeedback74@gmail.com>
2024-04-09 12:22:29 +0530

    Common resource added like Product review Recom class and Exception handle.

---
+++ microservice-core/src/main/java/com/core/microservicecore/composite/product/ServiceAddresses.java
@@ -0,0 +1,31 @@
+package com.core.microservicecore.composite.product;
+
+public class ServiceAddresses {
+    private final String cmp;
+    private final String pro;
+    private final String rev;
+    private final String rec;
+
+    public ServiceAddresses() {
+        cmp = null;
+        pro = null;
+        rev = null;
+        rec = null;
+    }
+
+    public ServiceAddresses(
+            String compositeAddress,
+            String productAddress,
+            String reviewAddress,
+            String recommendationAddress) {
+
+        this.cmp = compositeAddress;
+        this.pro = productAddress;
+        this.rev = reviewAddress;
+        this.rec = recommendationAddress;
+    }
+
+    public String getCmp() {
+        return cmp;
+    }
+}
